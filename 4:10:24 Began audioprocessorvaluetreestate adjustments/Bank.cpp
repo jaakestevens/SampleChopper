235117d08db849ec636874aad58dc768eb730e19
jaakestevens <99769150+jaakestevens@users.noreply.github.com>
2024-10-04 22:33:15 +0100

    Set up audioProcessor Value Tree State.
    Began to move variables into this value treeState so that they continue to exist as the editor dies.

---
+++ 4:10:24 Began audioprocessorvaluetreestate adjustments/Bank.cpp
@@ -0,0 +1,267 @@
+/*
+  ==============================================================================
+
+    Bank.cpp
+    Created: 6 Sep 2024 6:17:24pm
+    Author:  Jake
+
+  ==============================================================================
+*/
+
+#include "Bank.h"
+
+Bank::Bank(juce::AudioFormatManager& afm) : formatManager(afm)
+{
+    
+        transportSource.setSource(nullptr);
+        readerSource = nullptr;
+        
+        adsrParams.attack = 0.1f;  // Fast attack
+        adsrParams.decay = 0.0f;    // Short decay
+        adsrParams.sustain = 1.0f;  // Sustain level
+        adsrParams.release = 0.2f;  // Release time
+    
+        adsr.setParameters(adsrParams);
+    
+}
+
+Bank::~Bank()
+{
+   
+        transportSource.setSource(nullptr);
+        readerSource = nullptr;
+}
+
+void Bank::prepareToPlay(int samplesPerBlockExpected , double sampleRate)
+{
+  
+        transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+        resampleSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void Bank::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    
+    if(readerSource == nullptr)
+    {
+        return;
+    }
+    
+    auto l = loopRegion;
+    
+    if(l.proper())
+    {
+        auto p = getPositionRelative();
+        
+        if(p >= l.end()) //if reaches the end of loop region
+        {
+            setPositionRelative(l.start());
+            adsr.noteOff();
+            paused = true;
+        }
+        
+        if(p < l.start()) //before loop region, skips to the loop region
+        {
+            setPositionRelative(l.start());
+        }
+       
+    }
+    
+    if(!paused)
+    {
+        if(!readerSource.get())
+        {
+            bufferToFill.clearActiveBufferRegion();
+            return;
+        }
+        
+        
+        resampleSource.getNextAudioBlock(bufferToFill);
+        applyPan(bufferToFill, panPosition);
+        
+        
+        if(adsr.isActive())
+        
+        {
+            adsr.applyEnvelopeToBuffer(*bufferToFill.buffer, bufferToFill.startSample, bufferToFill.numSamples);
+           
+            
+        }
+        
+    }else
+    {
+        bufferToFill.clearActiveBufferRegion();
+    }
+}
+
+void Bank::releaseResources()
+{
+    transportSource.releaseResources();
+    resampleSource.releaseResources();
+}
+
+bool Bank::loadURL(const juce::URL& url)
+{
+    
+    auto * reader = formatManager.createReaderFor(url.getLocalFile());
+    
+    if(reader)
+    {
+        
+        std::unique_ptr <juce::AudioFormatReaderSource > newSource(new juce::AudioFormatReaderSource(reader , true));
+        
+        transportSource.setSource(newSource.get(), 0, nullptr , reader ->sampleRate);
+        
+        readerSource.reset(newSource.release());
+   
+        fileLoaded = true;
+        
+        return true;
+    }
+    return false;
+}
+
+void Bank::play()
+{
+    if(isListenerBank)
+    {
+        DBG("start listener");
+        adsr.reset();
+        adsr.noteOn();
+        transportSource.start();
+    }else
+    {
+        if(!isListenerBank && loopRegion.proper())
+        {
+            DBG("start loop");
+            adsr.reset();
+            adsr.noteOn();
+            
+            transportSource.setPosition(loopRegion.start());
+            transportSource.start();
+        }else
+        {
+            transportSource.stop();
+        }
+    }
+        
+        paused = false;
+    
+}
+void Bank::stop()
+{
+
+    transportSource.stop();
+ 
+}
+
+
+void Bank::setPosition(double posInSecs)
+{
+    if(posInSecs < 0. || posInSecs > transportSource.getLengthInSeconds())
+    {
+        DBG("Set position incorrect");
+        return;
+    }
+    
+    transportSource.setPosition(posInSecs);
+    
+    if(transportSource.isPlaying() == true)
+    {
+        transportSource.start();
+    }
+    
+}
+void Bank::setGain(double gain)
+{
+    float volume = gain / 5;
+    transportSource.setGain(volume);
+}
+
+bool Bank::isURLLoaded()
+{
+    return fileLoaded;
+}
+
+float Bank::getPositionRelative()
+{
+    auto length = transportSource.getLengthInSeconds();
+    
+    if(length > 0.f)
+    {
+        return transportSource.getCurrentPosition() / length;
+    }else
+    {
+        return 0.f;
+    }
+}
+
+void Bank::setPositionRelative(const double pos)
+{
+    auto posInSecs = pos * transportSource.getLengthInSeconds();
+    
+    setPosition(posInSecs);
+}
+
+void Bank::setLoopRegion(float start, float end)
+{
+        loopRegion.start(start); //between 0-1
+        loopRegion.end(end);
+        
+    if(!loopRegion.proper())
+        {
+            //not stopping transport source due to the overhead
+            DBG("loop stopped");
+            setPosition(transportSource.getLengthInSeconds());
+        }
+    
+}
+
+void Bank::setAdsrParameters(juce::ADSR::Parameters myParams)
+{
+    
+    adsr.setParameters(myParams);
+}
+
+juce::ADSR::Parameters* Bank::getAdsrParameters() //returning address
+{
+    return &adsrParams;
+}
+
+void Bank::setAdsrDisplay(bool state)
+{
+    adsrDisplayToggle = state;
+}
+
+bool Bank::getAdsrDisplay()
+{
+    return adsrDisplayToggle;
+}
+
+void Bank::setPanning(float panValue)
+{
+    panPosition = panValue;
+    DBG("Pan position = " << panPosition);
+}
+
+void Bank::applyPan(const juce::AudioSourceChannelInfo &bufferToFill, float panningVal)
+{
+    if(bufferToFill.buffer->getNumChannels() < 2) return;
+    
+    auto* leftChannel = bufferToFill.buffer->getWritePointer(0, bufferToFill.startSample);
+    auto* rightChannel = bufferToFill.buffer->getWritePointer(1, bufferToFill.startSample);
+    
+    const float leftGain = panningVal <= 0.0f ? 1.0f : 1.0f - panningVal; // more pan to the right, lower the left gain
+    const float rightGain = panningVal >= 0.0f ? 1.0f : 1.0f + panningVal;
+    
+    for(int sample = 0; sample < bufferToFill.numSamples; sample++)
+    {
+        leftChannel[sample] *= leftGain;
+        rightChannel[sample] *= rightGain;
+    }
+}
+void Bank::setSpeed(float speed)
+{
+    resampleSource.setResamplingRatio(speed);
+}
+
---
+++ 4:10:24 Began audioprocessorvaluetreestate adjustments/Sequencer.h
@@ -0,0 +1,76 @@
+/*
+  ==============================================================================
+
+    Sequencer.h
+    Created: 27 Sep 2024 10:05:28pm
+    Author:  Jake
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/*
+*/
+class Sequencer  : public juce::Component, public juce::Timer,  public juce::Button::Listener, public juce::Slider::Listener
+{
+public:
+    Sequencer();
+    ~Sequencer() override;
+
+    void paint (juce::Graphics&) override;
+    void resized() override;
+    void buttonClicked(juce::Button *button) override;
+    void sliderValueChanged(juce::Slider * slider) override;
+    void updateTimerInterval();
+    void timerCallback() override;
+    void setCurrentBank(int bank);
+
+    void start();
+    
+    void stop();
+    
+    std::function<void(int bank)>triggerSample;
+private:
+    
+    juce::TextButton seqStart{"Start"};
+    juce::TextButton increaseStepsButton{"+1 steps"};
+    juce::TextButton decreaseStepsButton{"-1 steps"};
+   
+    juce::Slider stepCountSlider;
+    std::vector<int> stepValues = {4, 8, 16, 32, 64};
+    
+    juce::Slider speedSlider;
+    std::vector<double> speedValues = {0.5, 1.0, 2.0, 4.0, 8.0, 16.0};
+    int currentSpeedIndex;
+    
+    juce::ToggleButton buttonHalf, buttonQuarter, buttonEighth, buttonSixteenth, buttonThirtySecond;
+    std::vector<juce::ToggleButton*> speedButtonVector = {&buttonHalf, &buttonQuarter, &buttonEighth, &buttonSixteenth, &buttonThirtySecond};
+    
+    juce::TextButton step1, step2, step3, step4, step5, step6, step7, step8, step9, step10, step11, step12, step13, step14, step15, step16;
+   
+    std::vector<juce::TextButton*>stepButtons = {&step1, &step2, &step3, &step4, &step5, &step6, &step7, &step8, &step9, &step10, &step11, &step12, &step13, &step14, &step15, &step16};
+    
+    std::vector<std::vector<int>> sequence = {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+    
+    int bpm = 120;
+    int stepsPerSequence = 16;
+    double stepDurationMs;
+    int currentStep = 0;
+    bool speedChange = false;
+    
+    bool sequencePlaying = true;
+    
+    int currentBank = 6;
+    
+    int calculateIntervalMs();
+    void processStep(int step);
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Sequencer)
+};
